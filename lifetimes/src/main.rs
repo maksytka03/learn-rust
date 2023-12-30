07d1a87557430f035e4576589d1a6e63f2a333ba
Pavlo <Pavlo>
2023-12-29 18:33:19 -0600

    Finished lifetimes

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,4 +1,26 @@
+use std::fmt::Display;
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str where T: Display, { // T implements Display
+    println!("Announcement! {}", ann);
     if x.len() > y.len() {
         x
     } else {
@@ -12,4 +34,10 @@ fn main() {
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find '.'");
+    let _i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
