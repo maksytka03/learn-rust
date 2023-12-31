30fac1a88de7b14a34ebf41aba9ff97264d2170a
Pavlo <dempavlo21@gmail.com>
2023-12-31 09:02:45 -0600

    Added 'adder', testing.

---
+++ adder/target/debug/deps/adder-9f7dce4b108e3fbc.d
@@ -0,0 +1,5 @@
+C:\Users\Pavlo\RustroverProjects\rust1\adder\target\debug\deps\adder-9f7dce4b108e3fbc.exe: src\lib.rs
+
+C:\Users\Pavlo\RustroverProjects\rust1\adder\target\debug\deps\adder-9f7dce4b108e3fbc.d: src\lib.rs
+
+src\lib.rs:
---
+++ adder/target/debug/deps/adder-fd099a56f8b88d25.d
@@ -0,0 +1,7 @@
+C:\Users\Pavlo\RustroverProjects\rust1\adder\target\debug\deps\adder-fd099a56f8b88d25.rmeta: src\lib.rs
+
+C:\Users\Pavlo\RustroverProjects\rust1\adder\target\debug\deps\libadder-fd099a56f8b88d25.rlib: src\lib.rs
+
+C:\Users\Pavlo\RustroverProjects\rust1\adder\target\debug\deps\adder-fd099a56f8b88d25.d: src\lib.rs
+
+src\lib.rs:
