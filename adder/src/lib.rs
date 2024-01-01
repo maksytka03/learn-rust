f0e79d941aabbe635d96b35d899f54260a998567
Pavlo <Pavlo>
2023-12-31 19:28:39 -0600

    Changes in 'adder'

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -14,6 +14,15 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    // format!("Hello, {}!", name)
+    String::from("Hello")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -36,6 +45,17 @@ mod tests {
     fn exploration() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(result.contains("Carol"), "Greeting did not contain name, value was '{}'", result);
     }
 
     #[test]
