28567a89f7bdfdb114fad029e31248f69d3a02d5
Pavlo <Pavlo>
2024-01-03 15:10:05 -0600

    Finished first version of 'minigrep'

---
+++ minigrep/target/debug/deps/minigrep-e769635045674f36.d
@@ -0,0 +1,5 @@
+/Users/dempa/RustroverProjects/rust1/minigrep/target/debug/deps/minigrep-e769635045674f36: src/main.rs
+
+/Users/dempa/RustroverProjects/rust1/minigrep/target/debug/deps/minigrep-e769635045674f36.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/deps/minigrep-fb56b25b827c1ca2.d
@@ -0,0 +1,7 @@
+/Users/dempa/RustroverProjects/rust1/minigrep/target/debug/deps/libminigrep-fb56b25b827c1ca2.rmeta: src/lib.rs
+
+/Users/dempa/RustroverProjects/rust1/minigrep/target/debug/deps/libminigrep-fb56b25b827c1ca2.rlib: src/lib.rs
+
+/Users/dempa/RustroverProjects/rust1/minigrep/target/debug/deps/minigrep-fb56b25b827c1ca2.d: src/lib.rs
+
+src/lib.rs:
