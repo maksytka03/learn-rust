5e8c1beda920904fb6bbde6ad905f46d289ee6f6
Pavlo <Pavlo>
2024-01-03 18:41:11 -0600

    Added case sensitivity through environment variables to 'minigrep'

---
+++ minigrep/target/debug/deps/minigrep-40ba3f3ff3bd14ea.d
@@ -0,0 +1,5 @@
+/Users/dempa/RustroverProjects/rust1/minigrep/target/debug/deps/minigrep-40ba3f3ff3bd14ea: src/lib.rs
+
+/Users/dempa/RustroverProjects/rust1/minigrep/target/debug/deps/minigrep-40ba3f3ff3bd14ea.d: src/lib.rs
+
+src/lib.rs:
---
+++ minigrep/target/debug/deps/minigrep-a97f04f52ad3ff07.d
@@ -0,0 +1,5 @@
+/Users/dempa/RustroverProjects/rust1/minigrep/target/debug/deps/minigrep-a97f04f52ad3ff07: src/main.rs
+
+/Users/dempa/RustroverProjects/rust1/minigrep/target/debug/deps/minigrep-a97f04f52ad3ff07.d: src/main.rs
+
+src/main.rs:
