0bee9daceb443d2ef198b6b429daf9bb276ebfc8
Pavlo <Pavlo>
2024-01-04 15:45:22 -0600

    Added 'closures'

---
+++ closures/target/debug/closures.d
@@ -0,0 +1,1 @@
+/Users/dempa/RustroverProjects/rust1/closures/target/debug/closures: /Users/dempa/RustroverProjects/rust1/closures/src/main.rs
---
+++ closures/target/debug/deps/closures-a997a7df1b4874df.d
@@ -0,0 +1,5 @@
+/Users/dempa/RustroverProjects/rust1/closures/target/debug/deps/closures-a997a7df1b4874df: src/main.rs
+
+/Users/dempa/RustroverProjects/rust1/closures/target/debug/deps/closures-a997a7df1b4874df.d: src/main.rs
+
+src/main.rs:
