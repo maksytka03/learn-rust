ba8bb8a8a48e4af974f3be5c8b300593b4741c4d
Pavlo <Pavlo>
2024-01-05 16:38:36 -0600

    Added closures and iterators to 'minigrep'

--- closures/target/debug/closures.d
+++ closures/target/debug/closures.d
@@ -1,1 +1,1 @@
-/Users/dempa/RustroverProjects/rust1/closures/target/debug/closures: /Users/dempa/RustroverProjects/rust1/closures/src/main.rs
+/Users/dempa/RustroverProjects/rust1/closures/target/debug/closures: /Users/dempa/RustroverProjects/rust1/closures/src/lib.rs /Users/dempa/RustroverProjects/rust1/closures/src/main.rs
