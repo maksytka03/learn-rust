882186decf6a0b4072f3327155e7caf0826089b4
Pavlo <Pavlo>
2024-01-11 19:39:39 -0600

    Changes in 'smart_pointers'.

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,4 +1,40 @@
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl <T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}")
+}
+
 fn main() {
-    let b = Box::new(5);
-    println!("b = {}", b);
+    let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y)
 }
---
+++ smart_pointers/target/debug/deps/smart_pointers-a5ea769d7d4b755a.d
@@ -0,0 +1,5 @@
+/Users/dempa/RustroverProjects/rust1/smart_pointers/target/debug/deps/smart_pointers-a5ea769d7d4b755a: src/main.rs
+
+/Users/dempa/RustroverProjects/rust1/smart_pointers/target/debug/deps/smart_pointers-a5ea769d7d4b755a.d: src/main.rs
+
+src/main.rs:
